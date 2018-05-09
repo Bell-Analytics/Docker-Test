c9794ab5e453ad34bf9f37b0dac1fbd31bc6d435
zw24code <37883105+zw24code@users.noreply.github.com>
2018-05-09 10:41:46 -0400

    Create panda_test.py

---
+++ docker/panda_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 11 18:27:54 2018
+
+@author: zachwerbell
+"""
+
+import pandas as pd
+
+# Build cars DataFrame
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+dict = { 'country':names, 'drives_right':dr, 'cars_per_cap':cpc }
+cars = pd.DataFrame(dict)
+print(cars)
